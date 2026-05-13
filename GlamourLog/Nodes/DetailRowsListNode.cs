7cb9b1c93560303bfc80c3aabf019ae5c56fda1f
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-05-13 11:14:55 +0100

    usings

--- GlamourLog/Nodes/DetailRowsListNode.cs
+++ GlamourLog/Nodes/DetailRowsListNode.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Extensions;
 using KamiToolKit.Nodes;
