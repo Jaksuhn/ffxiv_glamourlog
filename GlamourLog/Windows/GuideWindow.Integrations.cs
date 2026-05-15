4d127bcdb36eff659b215c79f624f645986a29fb
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-05-15 22:41:26 +0100

    Update GuideWindow.Integrations.cs

--- GlamourLog/Windows/GuideWindow.Integrations.cs
+++ GlamourLog/Windows/GuideWindow.Integrations.cs
@@ -14,10 +14,10 @@ public partial class GuideWindow {
                 .Append(" to get the amount of currency you have obtained in any inventory on\nyour character. If not installed, this count will default to your standard inventory only.\n\n")
                 .AddUiForeground(500).AddUiGlow(501).Append("vnavmesh").AddUiGlowOff().AddUiForegroundOff()
                 .Append(" is used in the context menu of ").AddUiForeground(710).Append("Sources and Costs.").AddUiForegroundOff()
-                .Append(" If loaded, this context menu entry will navigate you to the relevant source of the item.\n\n")
+                .Append(" If installed, this context menu entry will navigate you to the relevant source of the item.\n\n")
                 .AddUiForeground(500).AddUiGlow(501).Append("AutoDuty").AddUiGlowOff().AddUiForegroundOff()
-                .Append(" is used in the context menu of duty ").AddUiForeground(710).Append("Sources").AddUiForegroundOff()
-                .Append(" If loaded, this context menu entry will start an AutoDuty loop where your character\nwill run the relevant dungeon until all missing outfit pieces are acquired.")
+                .Append(" is used in the context menu of duty ").AddUiForeground(710).Append("Sources.").AddUiForegroundOff()
+                .Append(" If installed, this context menu entry will start an AutoDuty loop where your character\nwill run the relevant dungeon until all missing outfit pieces are acquired.")
                 .Encode()),
         BodyTextBoxHeight = 420f,
     };
