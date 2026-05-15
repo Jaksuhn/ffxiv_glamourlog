af996924a1cfed07ea35cadd9cd33c6706f173fd
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-05-15 22:35:07 +0100

    Update GuideWindow.Counters.cs

--- GlamourLog/Windows/GuideWindow.Counters.cs
+++ GlamourLog/Windows/GuideWindow.Counters.cs
@@ -7,13 +7,8 @@ public partial class GuideWindow {
         CategoryTitle = "Guide",
         SubCategoryTitle = "Counters",
         Body = new Lumina.Text.ReadOnly.ReadOnlySeString(
-                new SeStringBuilder()
-                .Append("The bottom-right of the glamour log displays two numbers.\n")
-                .Append("The top number is the amount of sets you have completed\n")
-                .Append("out of the total number of sets available in the game.\n\n")
-                .Append("The bottom number is the amount of glamour dresser slots\n")
-                .Append("you have\u00A0saved by having these pieces stored as outfits\n")
-                .Append("instead of being loose inside the dresser.\n")
+                new SeStringBuilder().Append("The bottom-right of the glamour log displays two numbers. The top number is the amount of sets you have completed out of the total number of sets available in the game.\n")
+                .Append("The bottom number is the amount of glamour dresser slots you have saved by having these pieces stored as outfits instead of being loose inside the dresser.\n\n")
                 .AddUiForeground(500).AddUiGlow(501).Append("※Completed counter combines sets in the dresser and the armoire").AddUiGlowOff().AddUiForegroundOff().Encode()),
         BodyTextBoxHeight = 220f,
     };
