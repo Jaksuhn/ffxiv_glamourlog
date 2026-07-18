b80c61276339f9ab27eeb783a61c7fbe1a4d3781
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-07-18 17:45:06 +0100

    le sigh

--- GlamourLog/Plugin.cs
+++ GlamourLog/Plugin.cs
@@ -25,7 +25,9 @@ public sealed class Plugin(IDalamudPluginInterface dalamud) : IAsyncDalamudPlugi
 
     public async Task LoadAsync(CancellationToken cancellationToken) {
         dalamud.Create<Svc>();
+#if LOCAL_CS
         dalamud.InitCustomClientStructs();
+#endif
         CLibMain.Init(dalamud, this, CLibModule.All);
         await Svc.Framework.RunOnFrameworkThread(() => KamiToolKitLibrary.Initialize(dalamud));
 
