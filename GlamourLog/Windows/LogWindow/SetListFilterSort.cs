77b7292d101e2a986a913c45c78ec486ddee36ff
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-09-18 11:14:36 +0100

    Update SetListFilterSort.cs

--- GlamourLog/Windows/LogWindow/SetListFilterSort.cs
+++ GlamourLog/Windows/LogWindow/SetListFilterSort.cs
@@ -74,62 +74,14 @@ internal static class SetListFilterSort {
     private static bool PassesClassJobFilter(GlamourSet set, IReadOnlyCollection<uint> classJobIds, bool partialMatch) {
         if (partialMatch)
             return classJobIds.Any(classJobId =>
-                set.Items.All(itemId => ClassJobCategoryContains(Item.GetRow(itemId).ClassJobCategory.Value, classJobId)));
+                set.Items.All(itemId => Item.GetRow(itemId).ClassJobCategory.Value.ContainsJob(ClassJob.GetRow(classJobId))));
 
         var selected = classJobIds.ToHashSet();
         return set.Items.All(itemId => {
             var category = Item.GetRow(itemId).ClassJobCategory.Value;
-            return Enumerable.Range(1, 42)
-                .All(classJobId => selected.Contains((uint)classJobId) == ClassJobCategoryContains(category, (uint)classJobId));
+            return ClassJob.All(job => selected.Contains(job.RowId) == category.ContainsJob(job));
         });
     }
-
-    private static bool ClassJobCategoryContains(ClassJobCategory category, uint classJobId)
-        => classJobId switch {
-            1 => category.GLA,
-            2 => category.PGL,
-            3 => category.MRD,
-            4 => category.LNC,
-            5 => category.ARC,
-            6 => category.CNJ,
-            7 => category.THM,
-            8 => category.CRP,
-            9 => category.BSM,
-            10 => category.ARM,
-            11 => category.GSM,
-            12 => category.LTW,
-            13 => category.WVR,
-            14 => category.ALC,
-            15 => category.CUL,
-            16 => category.MIN,
-            17 => category.BTN,
-            18 => category.FSH,
-            19 => category.PLD,
-            20 => category.MNK,
-            21 => category.WAR,
-            22 => category.DRG,
-            23 => category.BRD,
-            24 => category.WHM,
-            25 => category.BLM,
-            26 => category.ACN,
-            27 => category.SMN,
-            28 => category.SCH,
-            29 => category.ROG,
-            30 => category.NIN,
-            31 => category.MCH,
-            32 => category.DRK,
-            33 => category.AST,
-            34 => category.SAM,
-            35 => category.RDM,
-            36 => category.BLU,
-            37 => category.GNB,
-            38 => category.DNC,
-            39 => category.RPR,
-            40 => category.SGE,
-            41 => category.VPR,
-            42 => category.PCT,
-            _ => false,
-        };
 
     private static bool MatchesSearch(GlamourSet set, string searchTrimmed)
         => set.Name.Contains(searchTrimmed, StringComparison.OrdinalIgnoreCase)
